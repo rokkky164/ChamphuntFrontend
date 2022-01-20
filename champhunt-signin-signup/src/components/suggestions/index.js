4e10a79484fe98ed176d60e6d373af6e89b2bcf9
Sujith Patil <emailsp019@gmail.com>
2022-01-20 17:21:36 +0530

    adding suggestion component

--- champhunt-signin-signup/src/components/suggestions/index.js
+++ champhunt-signin-signup/src/components/suggestions/index.js
@@ -1,8 +1,46 @@
+import { useEffect, useState } from 'react';
 import './index.scss';
+import Suggestion from './suggestion';
 
 const Suggestions = () => {
+
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(()=>{
+        const data = [
+            {
+                name: "Pranav Karwa",
+                role: "Bowler",
+                avatar: "https://i.pravatar.cc/75",
+                followers: 55,
+                runs: 300,
+                id: 1
+            },{
+                name: "Rajatesh Nath",
+                role: "Umpire",
+                avatar: "https://i.pravatar.cc/75",
+                followers: 55,
+                runs: 120,
+                id: 2
+            },{
+                name: "Sameer Kanva",
+                role: "Batsmen",
+                avatar: "https://i.pravatar.cc/75",
+                followers: 15,
+                runs: 3000,
+                id: 3
+            }
+        ];
+
+        setSuggestions(data);
+
+    }, []);
+
     return <div className="component suggestions">
-        SUGGESTIONS
+        <p className='suggest-heading'>
+            Suggestions
+        </p>
+        { suggestions.map( suggestion => <Suggestion key={suggestion.id} {...suggestion} /> ) }
     </div>
 }
 
