70b48fd0d84ec189e1d04494e086b89989529872
Sujith Patil <emailsp019@gmail.com>
2022-01-20 18:11:55 +0530

    adding hero component

--- champhunt-signin-signup/src/components/feeds/index.js
+++ champhunt-signin-signup/src/components/feeds/index.js
@@ -1,3 +1,6 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+
 import Alert from "../alerts";
 import Hero from "../hero";
 import Posts from "../posts";
@@ -5,10 +8,33 @@ import Posts from "../posts";
 import './index.scss';
 
 const Feeds = () => {
+
+    const value = 1;
+
+    const handleChange = () => {
+
+    }
+
     return <div className="component feeds">
         <Alert />
-        <Hero />
-        <Posts />
+        <Tabs className="tabs">
+            <TabList>
+                <Tab>All</Tab>
+                <Tab>My posts</Tab>
+                <Tab>By friends</Tab>
+            </TabList>
+
+            <TabPanel>
+                <Hero />
+                <Posts />
+            </TabPanel>
+            <TabPanel>
+                <h2>Any content 2</h2>
+            </TabPanel>
+            <TabPanel>
+                <h2>Any content 2</h2>
+            </TabPanel>
+        </Tabs>
     </div>
 }
 
--- champhunt-signin-signup/src/components/hero/index.js
+++ champhunt-signin-signup/src/components/hero/index.js
@@ -1,6 +1,18 @@
+import Virat from '../../assets/images/header/virat.png';
+
+import './index.scss';
+
 const Hero = () => {
     return <div className="component hero">
-        HERO
+        <div className='hero-cnt'>
+            <div className='image-block'>
+                <img src={Virat} alt='Virat Kohli' />
+            </div>
+            <p className='hero-content'>
+                Virat Kohli becomes the 3rd Indian skipper to hit three consecutive world cup fifties.
+                Mohd. Azaruddin was the first in the 1992 world cup. 
+            </p>
+        </div>
     </div>
 }
 
