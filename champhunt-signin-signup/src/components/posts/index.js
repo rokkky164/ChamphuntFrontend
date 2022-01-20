72063655cf31257889adbac5f30e8660bc634525
Sujith Patil <emailsp019@gmail.com>
2022-01-20 18:55:45 +0530

    Adding posts component

--- champhunt-signin-signup/src/components/posts/index.js
+++ champhunt-signin-signup/src/components/posts/index.js
@@ -1,6 +1,55 @@
+import { useEffect, useState } from "react";
+
+import Post from './post';
+
+import './index.scss';
+
 const Posts = () => {
+
+    const[posts, setPosts] = useState([]);
+
+    useEffect(()=>{
+        const data = [
+            {
+                author: {
+                    name: "Sameer Kanva",
+                    url: "/users/chandan",
+                    avatar: "https://i.pravatar.cc/45",
+                },
+                coAuthor: {
+                    name: "Navendhu Sinha",
+                    url: "/users/navendhu",
+                },
+                post: {
+                    date: "30.12.2021",
+                    time: "15:20:45",
+                    content: "Hey, Stats of a winner",
+                    comments: 4,
+                    runs: 123
+                }
+            },{
+                author: {
+                    name: "Navendhu Sinha",
+                    url: "/users/chandan",
+                    avatar: "https://i.pravatar.cc/45",
+                },
+                post: {
+                    date: "30.12.2021",
+                    time: "15:20:45",
+                    content: "Sachin vs Kohli",
+                    comments: 4,
+                    runs: 123
+                }
+            }
+        ];
+
+        setPosts(data);
+    }, []);
+
     return <div className="component posts">
-        Posts
+        {
+            posts.map((post, index) => <Post key={index} {...post} />)
+        }
     </div>
 }
 
---
+++ champhunt-signin-signup/src/components/posts/post.js
@@ -0,0 +1,56 @@
+const Post = ( props ) => {
+
+    const { 
+        author: {
+            name,
+            url,
+            avatar
+        },
+        coAuthor,
+        post: {
+            date,
+            time,
+            content,
+            comments,
+            runs
+        }
+    } = props;
+
+    return <div className="post">
+        <div className="post-header">
+            <div className="left">
+                <div className="avatar">
+                    <img className="avatar-image" src={avatar} alt={name} />
+                </div>
+                <div className="avatar-cnt">
+                    <p>{ name }</p>
+                    <p>{ date } {time}</p>
+                </div>
+            </div>
+            <div className="right">
+                <div className="icon-holder">
+
+                </div>
+                <div className="runs-cnt">
+                    {runs} runs
+                </div>
+            </div>
+        </div>
+        <div className="post-content">
+            {content}
+        </div>
+        <div className="post-footer">
+            <div className="comments-hld">
+                CMN
+            </div>
+            <div className="share-hld">
+                SHR
+            </div>
+            <div className="runs-hld">
+                RUNS
+            </div>
+        </div>
+    </div>
+}
+
+export default Post;
