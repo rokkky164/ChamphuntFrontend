4e141914dcb2dc028222c664cab968c9087a0dec
Sujith Patil <emailsp019@gmail.com>
2022-02-03 19:10:51 +0530

    Adding share functionality of post

--- champhunt-signin-signup/src/components/posts/post.js
+++ champhunt-signin-signup/src/components/posts/post.js
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import ChampButton from '../../commons/form/button';
 
 import Comments from '../../assets/images/posts/comments.svg';
 import Share from '../../assets/images/posts/share.svg';
@@ -6,7 +9,6 @@ import Runs from '../../assets/images/posts/runs.svg';
 import InfoIcon from '../../assets/images/posts/info.svg';
 
 import './index.scss';
-import { useState } from 'react';
 
 const Post = ( props ) => {
 
@@ -31,6 +33,7 @@ const Post = ( props ) => {
     } = props;
 
     const [showRuns, setShowRuns] = useState(false);
+    const [sharing, setSharing] = useState(false);
 
     const toggleRuns = () => {
         setShowRuns(!showRuns);
@@ -38,6 +41,25 @@ const Post = ( props ) => {
 
     const scorePost = (run) => {
         console.log( run );
+    }
+
+    const showModal = () => {
+        setSharing(true);
+    }
+
+    const copyToClipboard = () => {
+        const copyText = document.querySelector("#referId");
+        copyText.select();
+        document.execCommand("copy");
+        closeModal();
+    }
+
+    const closeModal = () => {
+        setSharing(false);
+    }
+
+    const sharePost = () => {
+        console.log('CLOSING MODAL');
     }
 
     return <div className="post">
@@ -66,13 +88,57 @@ const Post = ( props ) => {
         <div className="post-content">
             {content}
         </div>
-        <div className="post-footer">
+        <div className="post-footer post-header">
             <div className="comments-hld">
                 {comments > 0 && <span className='info-box'>{comments}</span> }
                 <img src={Comments} alt='' />
             </div>
             <div className="share-hld">
-                <img src={Share} alt='' />
+                <img src={Share} alt='' role='button' onClick={showModal} />
+                <div className={`share-modal ${sharing?'visible':'hidden'}`}>
+                    <div className='share-modal__container'>
+                        <div className='share-modal__container__form'>
+                            <div className='input-textarea'>
+                                <textarea placeholder='Write Something'></textarea>
+                            </div>
+                            <div className="content left">
+                                <div className="avatar">
+                                    <img className="avatar-image" src={avatar} alt={name} />
+                                <div className="avatar-cnt">
+                                    <p>
+                                        { name }
+                                    </p>
+                                    <p className='date-time'>{ date } {time}</p>
+                                </div>
+                                </div>
+                                
+                                <p className='content' >{content}</p>
+                            </div>
+                        </div>
+                        <div className='share-modal__container__actions'>
+                            <ChampButton onClick = {closeModal} classes='cancel secondary' label='Cancel' />
+                            <ChampButton onClick = {sharePost} classes='share primary' label='Share' />
+                        </div>
+                        <div className='share-modal__container__more'>
+                            <div className="link-block">
+                                <p className='info'>
+                                More ways to invite your friends
+                                </p>
+                                <div className='copy-link-cnt'>
+                                    <div className='copy-btn-cnt'>
+                                        <img className='link' src={Link} alt='' />
+                                        <button onClick={copyToClipboard} className='copy'>
+                                            Copy link
+                                        </button>
+                                    </div>
+                                    <div className='copy-text-cnt'>
+                                        <input className='refer-id' id='referId' value='https://ch.tt/LC9652T' readOnly={true} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className="runs-hld">
                 <img src={Runs} alt='' role='button' onClick={toggleRuns} />
