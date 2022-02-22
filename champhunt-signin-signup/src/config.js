144753205d5beeed98477ec1b404784494c7d7db
lenovo <roshan@quantumsds.com>
2022-02-23 00:50:07 +0530

    changed prod url

--- champhunt-signin-signup/src/config.js
+++ champhunt-signin-signup/src/config.js
@@ -1,11 +1,11 @@
 module.exports = global.config = {
     ROOTURL: {
         local: 'http://localhost:8001',
-        prod: 'http://champhunt-sm-01.eba-zezpix24.us-west-1.elasticbeanstalk.com'
+        prod: 'http://champhunt-sm-02.eba-zezpix24.us-west-1.elasticbeanstalk.com'
     },
     WSURLS : {
         local: 'ws://localhost:8001',
-        prod: 'ws://champhunt-sm-01.eba-zezpix24.us-west-1.elasticbeanstalk.com'
+        prod: 'ws://champhunt-sm-02.eba-zezpix24.us-west-1.elasticbeanstalk.com'
     },
     ROOTURL_FOR_REACT: {
         local: 'http://localhost:3000',
