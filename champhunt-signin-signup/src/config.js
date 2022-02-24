ed8454a77559945702cc5ad99a9cf8164fc50089
rokkky164 <hansdah.roshan@gmail.com>
2022-02-25 00:55:47 +0530

    prod url modf

--- champhunt-signin-signup/src/config.js
+++ champhunt-signin-signup/src/config.js
@@ -1,11 +1,11 @@
 module.exports = global.config = {
     ROOTURL: {
         local: 'http://localhost:8001',
-        prod: 'http://champhunt-sm-01.eba-zezpix24.us-west-1.elasticbeanstalk.com'
+        prod: 'http://champhuntsm-env.eba-zezpix24.us-west-1.elasticbeanstalk.com'
     },
     WSURLS : {
         local: 'ws://localhost:8001',
-        prod: 'ws://champhunt-sm-01.eba-zezpix24.us-west-1.elasticbeanstalk.com'
+        prod: 'ws://champhuntsm-env.eba-zezpix24.us-west-1.elasticbeanstalk.com'
     },
     ROOTURL_FOR_REACT: {
         local: 'http://localhost:3000',
